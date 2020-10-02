9743551a0114dc34610a92869c1b9bdd9c6c2a55
Atul1023 <07atul1023@gmail.com>
2020-10-02 14:47:42 +0530

    Added appbar and a static homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,5 +2,41 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: Colors.green,
+        title: Text("KIIT FOSS Community"),
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          child: Card(
+            color: Colors.green[400],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.warning,
+                  color: Colors.red,
+                ),
+                Text(
+                  "<<<Work at progress>>>",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                Icon(
+                  Icons.warning,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
